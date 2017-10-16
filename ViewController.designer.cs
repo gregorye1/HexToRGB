4cdb1d5b78b47b29d176170ee6e2463c7954c0b9
gregorye1 <32143199+gregorye1@users.noreply.github.com>
2017-10-16 13:31:38 -0600

    added name

--- ViewController.designer.cs
+++ ViewController.designer.cs
@@ -1,17 +1,64 @@
-﻿//
-// This file has been generated automatically by MonoDevelop to store outlets and
-// actions made in the Xcode designer. If it is removed, they will be lost.
-// Manual changes to this file may not be handled correctly.
+// WARNING
+//
+// This file has been generated automatically by Visual Studio from the outlets and
+// actions declared in your storyboard file.
+// Manual changes to this file will not be maintained.
 //
 using Foundation;
+using System;
+using System.CodeDom.Compiler;
 
 namespace HextoRGB
 {
-    [Register("ViewController")]
+    [Register ("ViewController")]
     partial class ViewController
     {
-        void ReleaseDesignerOutlets()
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UILabel blueValue { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UIButton Convert { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UILabel greenValue { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UITextField hexValueTextField { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UILabel redValue { get; set; }
+
+        void ReleaseDesignerOutlets ()
         {
+            if (blueValue != null) {
+                blueValue.Dispose ();
+                blueValue = null;
+            }
+
+            if (Convert != null) {
+                Convert.Dispose ();
+                Convert = null;
+            }
+
+            if (greenValue != null) {
+                greenValue.Dispose ();
+                greenValue = null;
+            }
+
+            if (hexValueTextField != null) {
+                hexValueTextField.Dispose ();
+                hexValueTextField = null;
+            }
+
+            if (redValue != null) {
+                redValue.Dispose ();
+                redValue = null;
+            }
         }
     }
 }
