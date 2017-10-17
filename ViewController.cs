43b449deba5a8b6a751292674a4a00b1c3421ed9
gregorye1 <32143199+gregorye1@users.noreply.github.com>
2017-10-17 13:11:29 -0600

    reload

--- ViewController.cs
+++ ViewController.cs
@@ -16,27 +16,26 @@ namespace HextoRGB
             base.ViewDidLoad();
             // Perform any additional setup after loading the view, typically from a nib.
 
-            convertButton.TouchUpInside += Convert_TouchUpInside;
+            convertButton.TouchUpInside += ConvertButton_TouchUpInside;
         }
 
-        void Convert_TouchUpInside(object sender, EventArgs e)
+        void ConvertButton_TouchUpInside(object sender, EventArgs e)
         {
-            //These strings are to assign the numbers to what values
-            string hexValue = hexValueTextField.Text;
-            string redHexValue = hexValue.Substring(0, 2);
-            string greenHexValue = hexValue.Substring(2, 2);
-            string blueHexValue = hexValue.Substring(4, 2);
+			//These strings are to assign the numbers to what values
+			string hexValue = hexValueTextField.Text;
+			string redHexValue = hexValue.Substring(0, 2);
+			string greenHexValue = hexValue.Substring(2, 2);
+			string blueHexValue = hexValue.Substring(4, 2);
 
-            int redValue = int.Parse(redHexValue, System.Globalization.NumberStyles.HexNumber);
-            int greenValue = int.Parse(greenHexValue, System.Globalization.NumberStyles.HexNumber);
-            int blueValue = int.Parse(blueHexValue, System.Globalization.NumberStyles.HexNumber);
+			int redValue = int.Parse(redHexValue, System.Globalization.NumberStyles.HexNumber);
+			int greenValue = int.Parse(greenHexValue, System.Globalization.NumberStyles.HexNumber);
+			int blueValue = int.Parse(blueHexValue, System.Globalization.NumberStyles.HexNumber);
 
-            redValueLabel.Text = redValue.ToString();
-            greenValueLabel.Text = greenValue.ToString();
-            blueValueLabel.Text = blueValue.ToString();
+			redValueLabel.Text = redValue.ToString();
+			greenValueLabel.Text = greenValue.ToString();
+			blueValueLabel.Text = blueValue.ToString();
 
-            colorView.BackgroundColor = UIColor.FromRGB(redValue, greenValue, blueValue);
-                 
+			colorView.BackgroundColor = UIColor.FromRGB(redValue, greenValue, blueValue);
         }
 
         public override void DidReceiveMemoryWarning()
